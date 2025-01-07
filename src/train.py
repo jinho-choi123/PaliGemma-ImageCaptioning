a17adf6c51d68d501c5619444913c2ce95983394
jinho-choi123 <choijinho817@gmail.com>
2025-01-07 20:14:32 +0900

    fix typo

--- src/train.py
+++ src/train.py
@@ -29,7 +29,7 @@ lora_config = LoraConfig(
         )
 
 if config.get("load_lora", False):
-    model = AutoPeftModel.from_pretrained(config.get("lora_checkpoint_repo_id"), quantization_config=bnb_config)
+    model = AutoPeftModel.from_pretrained(config.get("hf_checkpoint_repo_id"), quantization_config=bnb_config)
 else:
     model = PaliGemmaForConditionalGeneration.from_pretrained(config.get("pretrained_repo_id"), quantization_config=bnb_config)
     model = get_peft_model(model, lora_config)
