933ca7d97d42cc23cb1ce0d4c376bbea9a2f42ed
jinho-choi123 <61547655+jinho-choi123@users.noreply.github.com>
2025-01-08 17:29:43 +0900

    Update inference.py
    add logging in inference step

--- src/inference.py
+++ src/inference.py
@@ -9,19 +9,22 @@ from transformers import AutoProcessor
 processor = AutoProcessor.from_pretrained(config.get("pretrained_repo_id"))
 
 # IMG_PATH = "path/to/image.jpg"
-IMG_PATH = "examples/example1.jpg"
+IMG_PATH = "examples/example.jpg"
 images = [Image.open(IMG_PATH)]
 prompts = ["<image>Caption for the image is "]
 
 
 
 model = PaliGemmaForConditionalGeneration.from_pretrained(config.get("pretrained_repo_id"))
+print(f"Loaded PaliGemma model...")
 
 # set the is_trainable flag as False because we are doing inference-only
 model = PeftModel.from_pretrained(model, config.get("hf_checkpoint_repo_id"), is_trainable=False)
+print(f"Loaded peft adapter for PaliGemma model...")
 
 with torch.no_grad():
     inference_inputs = processor(text=prompts, images=images, return_tensors="pt")
+    print(f"processing the input image completed...")
     input_ids = inference_inputs["input_ids"]
     attention_mask = inference_inputs["attention_mask"]
     pixel_values = inference_inputs["pixel_values"]
@@ -32,6 +35,8 @@ with torch.no_grad():
             pixel_values=pixel_values,
             max_new_tokens=config.get("max_new_tokens")
             )
+
+    print(f"Finished generating the predictions...")
 
     predictions = processor.batch_decode(generated_ids[:, input_ids.size(1)+1:], skip_special_tokens=True)
     
